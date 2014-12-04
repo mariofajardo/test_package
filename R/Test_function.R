3f0b95b2f7ec20a3f568ceb99cada18c0985ecf6
Mario Fajardo <mario.fajardopedraza@sydney.edu.au>
2014-12-04 14:35:11 +1100

    testing

---
+++ R/Test_function.R
@@ -0,0 +1,8 @@
+#' Prints hello
+#' 
+#' Takes an argument and prints it to the f!#$ing screen
+#' @param a character vector
+#' @return a character vector
+#' @export 
+
+Test_function <- function(x) print(x)
--- R/test_package.R
+++
@@ -1,8 +0,0 @@
-#' Prints hello
-#' 
-#' Takes nothing and gives you 'hello R!
-#' @param x Nothing
-#' @return hello R
-#' @export
-
-hello <- function(x) print(x)
